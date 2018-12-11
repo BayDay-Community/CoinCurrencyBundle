e68cfc58c4e7a30ea5486f0cedb77889bd6c5f38
Donjohn <Donjohn@users.noreply.github.com>
2018-12-11 12:10:03 +0100

    fixing mapping folder

--- BayDayCoinCurrencyBundle.php
+++ BayDayCoinCurrencyBundle.php
@@ -9,8 +9,18 @@ declare(strict_types=1);
 
 namespace BayDay\CoinCurrencyBundle;
 
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class BayDayCoinCurrencyBundle extends Bundle
 {
+
+    public function build(ContainerBuilder $container)
+    {
+        if (class_exists('Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass')) {
+            $container->addCompilerPass( DoctrineOrmMappingsPass::createAnnotationMappingDriver( array('BayDay\CoinCurrencyBundle\Model') , array(realpath(__DIR__.DIRECTORY_SEPARATOR.'Model')) ));
+        }
+
+    }
 }
